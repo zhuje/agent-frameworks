8a68b72989373d985f2b668d3fe5bb29d9c0f939
Ann Marie Fred <afred@redhat.com>
2025-03-25 12:40:05 -0400

    updates typical llama stack port and uses env variables if set

--- prototype/frameworks/llamastack/scripts/mcp-tool-agent.py
+++ prototype/frameworks/llamastack/scripts/mcp-tool-agent.py
@@ -1,3 +1,4 @@
+import os
 from llama_stack_client.lib.agents.agent import Agent
 from llama_stack_client.lib.agents.event_logger import EventLogger
 from llama_stack_client.types.agent_create_params import AgentConfig
@@ -6,13 +7,14 @@ from termcolor import cprint
 
 ## Start the llama stack server with vLLM backend
 
+# export LLAMA_STACK_PORT=8321
 # podman run \
 #   -it \
-#   -p 5001:5001 \
+#   -p $LLAMA_STACK_PORT:$LLAMA_STACK_PORT \
 #   -v ./run.yaml:/root/my-run.yaml \
 #   llamastack/distribution-remote-vllm \
 #   --yaml-config /root/my-run.yaml \
-#   --port 5001 \
+#   --port $LLAMA_STACK_PORT \
 #   --env INFERENCE_MODEL=meta-llama/Llama-3.2-3B-Instruct \
 #   --env VLLM_URL=xxx
 
@@ -20,10 +22,12 @@ from termcolor import cprint
 # git clone https://github.com/modelcontextprotocol/python-sdk
 # Follow instructions to get the env ready
 # cd examples/servers/simple-tool
-# uv run mcp-simple-tool --transport sse --port 8000
+# export MCP_PORT=8000
+# uv run mcp-simple-tool --transport sse --port $MCP_PORT
 
 # Connect to the llama stack server
-base_url="http://localhost:5001"
+llama_stack_port = os.environ.get("LLAMA_STACK_PORT", "8321")
+base_url=f"http://localhost:{llama_stack_port}"
 model_id="meta-llama/Llama-3.2-3B-Instruct"
 client = LlamaStackClient(base_url=base_url)
 
@@ -31,10 +35,11 @@ client = LlamaStackClient(base_url=base_url)
 # Register MCP tools
 # If you're running llamastack through conda 
 # then http://localhost:8000/sse will work for mcp_endpoint
+mcp_port = os.environ.get("MCP_PORT", "8000")
 client.toolgroups.register(
     toolgroup_id="mcp::filesystem",
     provider_id="model-context-protocol",
-    mcp_endpoint={"uri":"http://host.containers.internal:8000/sse"})
+    mcp_endpoint={"uri":f"http://host.containers.internal:{mcp_port}/sse"})
 
 # Define an agent with MCP toolgroup 
 agent_config = AgentConfig(
